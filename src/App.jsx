b5e03eb90828deef618e0f4f505f1d5037841ad4
Alexei <>
2024-02-14 16:35:22 +0200

    2.7: puhelinluettelo step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import Person from './components/Person';
 
 const App = () => {
+
     const [persons, setPersons] = useState([
-        {name: 'Arto Hellas'}
+        { name: 'Arto Hellas' }
     ]);
     const [newName, setNewName] = useState('');
 
@@ -13,18 +14,23 @@ const App = () => {
 
     const addPerson = (event) => {
         event.preventDefault(); // Tämä estää lomakkeen oletusarvoisen toiminnan
+        const nameExists = persons.some(person => person.name === newName);
+        if (nameExists) {
+            alert(`${newName} is already added to the phonebook.`);
+            return;
+        }
+        const newPerson = { name: newName };
 
-        const newPerson = {name: newName};
         setPersons(persons.concat(newPerson));
         setNewName('');
     };
-
+    console.log(persons);
     return (
         <div>
             <h2>Phonebook</h2>
             <form onSubmit={addPerson}>
                 <div>
-                    name: <input value={newName} onChange={handleNameChange}/>
+                    name: <input value={newName} onChange={handleNameChange} />
                 </div>
                 <div>
                     <button type="submit">add</button>
@@ -33,7 +39,7 @@ const App = () => {
             <h2>Numbers</h2>
             {persons.map((person) => (
                 // Kentän key-arvona käytetään henkilön nimeä
-                <Person key={person.name} person={person}/>
+                <Person key={person.name} person={person} />
             ))}
         </div>
     );
