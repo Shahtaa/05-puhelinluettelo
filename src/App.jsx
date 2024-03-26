4796da60f08be4ca292fe290c9e7853c17e64be3
Alexei <>
2024-03-26 10:02:00 +0200

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ const App = () => {
             setErrorMessage(null);
         }, 3000); // Show the message for 3 seconds
     };
+
     useEffect(() => {
         personService.getAll()
             .then(data => {
